25db31217b968a5c51de6ba43b5fee60a49db3c0
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2018-04-10 11:35:14 -0500

    Imported desired functions (bookmark/list directories; summarize cmd_history)
    Taken from [OMF's budspencer theme](https://github.com/oh-my-fish/theme-budspencer/blob/master/fish_prompt.fish)

---
+++ etc/fish/functions/mark.fish
@@ -0,0 +1,13 @@
+﻿#!/usr/bin/fish
+
+function mark -d 'Add arguments to bookmarks.'
+  builtin test (builtin count $argv) -lt 1 -o ! -d $argv;
+    and mark (command pwd);
+    and return 0
+  for i in $argv
+    builtin test ! (builtin contains $i $bookmarks);
+      and set -g bookmarks $i $bookmarks;
+      and printf '%s%s%s%s bookmarked.%s\n' $bold $cyan $i $white $normal
+  end
+end
+
---
+++ etc/fish/functions/unmark.fish
@@ -0,0 +1,13 @@
+﻿#!/usr/bin/fish
+
+function unmark -d 'remove argument from bookmarks'
+  builtin test (builtin count $argv) -lt 1 -o ! -d $argv;
+    and unmark (command pwd);
+    and return 0
+  for i in $argv
+    builtin test (builtin contains $i $bookmarks);
+      and set -e bookmarks[(builtin contains -i $i $bookmarks)];
+      and printf '%s%sBookmark %s%s%s removed.%s\n' $bold $white $cyan $i $white $normal
+  end
+end
+
