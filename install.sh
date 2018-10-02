27c2c5dab1f26b76e2eeec12b8e6b7f1ed5ee325
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2018-10-02 10:22:32 -0500

    Added external.list to installation file

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ for i in $(cat $MYCONFIG_DIR/apt.key); do
   wget -v -O - $i | sudo apt-key add -
 done
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6494C6D6997C215E
-sudo echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb stable main' | sudo tee -a /etc/apt/sources.list.d/google-chrome.list
-sudo echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_18.04/ /' | sudo tee -a /etc/apt/sources.list.d/home:manuelschneid3r.list
-sudo wget -v -O /usr/local/bin/googler https://raw.githubusercontent.com/jarun/googler/v3.6/googler && sudo chmod +x /usr/local/bin/googler
+sudo ln -v $MYCONFIG_DIR/etc/apt/sources.list.d/external.list
+sudo wget -v -O /usr/local/bin/googler https://raw.githubusercontent.com/jarun/googler/v3.6/googler
+sudo chmod +x /usr/local/bin/googler
 for i in $(cat $MYCONFIG_DIR/dpkg.apt.sources)
   sudo add-apt-repository -y $i
 done
