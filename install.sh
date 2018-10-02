b83d87b720230145d84d2be78073d954e04fb3c5
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2018-10-02 10:55:20 -0500

    Corrected typos

--- install.sh
+++ install.sh
@@ -1,41 +1,48 @@
 #!/bin/bash
 # Run /path/to/repo/rehome first!
 sudo updatedb
-set MYCONFIG_DIR=$(dirname $(locate -eq 'my-config/.git' | head -1))
-set GIT_DIR=$(dirname $MYCONFIG_DIR)
+set MYCONFIG_DIR=$(command dirname $(locate -eq 'my-config/.git' | command head -1))
+set GIT_DIR=$(command dirname $MYCONFIG_DIR)
 
-for i in $(cat $MYCONFIG_DIR/apt.key)
+for i in $(command cat $MYCONFIG_DIR/apt.key)
 do
-  wget -v -O - $i | sudo apt-key add -
+  command wget -v -O - $i | sudo apt-key add -
 done
-for i in $(tr '_' ' ' $MYCONFIG_DIR/custom-apt.key)
+
+for i in $(command cat $MYCONFIG_DIR/custom-apt.key)
 do
-  sudo apt-key adv --keyserver $(tr )
+  builtin println '%s' $i | command read -d'_' server key
+  sudo apt-key adv --keyserver $server --recv-keys $key
 done
+
 sudo ln -v $MYCONFIG_DIR/etc/apt/sources.list.d/external.list /etc/apt/sources.list.d/external.list
 sudo wget -v -O /usr/local/bin/googler https://raw.githubusercontent.com/jarun/googler/v3.6/googler
 sudo chmod +x /usr/local/bin/googler
-for i in $(cat $MYCONFIG_DIR/dpkg.apt.sources)
+
+for i in $(command cat $MYCONFIG_DIR/dpkg.apt.sources)
 do
   sudo add-apt-repository -y $i
 done
 
 sudo debconf-set-selections $MYCONFIG_DIR/apt.debconf
+
 for "update"
     "autoremove"
 do
   sudo apt $i -y
 done
-sudo apt install -y $(cat $MYCONFIG_DIR/dpkg.apt)
-sudo apt purge -y $(cat $MYCONFIG_DIR/rdpkg.apt)
+
+sudo apt install -y $(command cat $MYCONFIG_DIR/dpkg.apt)
+sudo apt purge -y $(command cat $MYCONFIG_DIR/rdpkg.apt)
+
 for "upgrade"
     "clean"
 do
   sudo apt $i -y
 done
 
-sudo snap install $(cat $MYCONFIG_DIR/dpkg.snap)
-sudo pip3 install -vvv $(cat $MYCONFIG_DIR/dpkg.pip3)
+sudo snap install $(command cat $MYCONFIG_DIR/dpkg.snap)
+sudo pip3 install -vvv $(command cat $MYCONFIG_DIR/dpkg.pip3)
 
 for i in $(cat $MYCONFIG_DIR/dpkg.git)
 do
