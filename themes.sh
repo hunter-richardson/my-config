8f25683eb8c3fcec771789f1b8f69caa4b549945
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2018-10-04 09:10:54 -0500

    Removed sudo where it's unnecessary

--- themes.sh
+++ themes.sh
@@ -1,6 +1,6 @@
 # download https://opendesktop.org/c/1460734834
 cd ~/Downloads
-sudo dtrx -nv ~/Downloads/163336-DMZhaloRP.tar.gz
+dtrx -nv ~/Downloads/163336-DMZhaloRP.tar.gz
 sudo mkdir -p /usr/share/icons/DMZhaloR32
 sudo rsync -ADhorX ~/Downloads/163336-DMZhaloRP/DMZhaloRP/DMZhaloR32/* /usr/share/icons/DMZhaloR32/
 sudo ln -fs /usr/share/icons/DMZhaloR24/cursor.theme /etc/alternatives/x-cursor-theme
