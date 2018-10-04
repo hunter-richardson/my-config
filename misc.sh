f2500893c6257ed49787cc0ade9111e30fcb04cb
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2018-10-04 09:13:41 -0500

    Correct installation typos and consistency

--- misc.sh
+++ misc.sh
@@ -4,12 +4,9 @@ sudo updatedb
 set MYCONFIG_DIR=$(dirname $(locate -eq 'my-config/.git' | head -1))
 
 sudo tlp start
-cd $(dirname $MYCONFIG_DIR)/terminal-slack
-sudo npm install
 cd $MYCONFIG
 sudo $(dirname $MYCONFIG_DIR)/matcha/Install
 
-sudo apt install -y lightdm unity-greeter
 sudo xinput set-button-map 11 1 2 3 4 5 6 7 0 0
 
 for i in "fish/functions"
@@ -24,29 +21,23 @@ done
 for i in "etc/skel/.config/git/config"
          "etc/tmux.conf"
          "usr/local/sbin/adduser.local"
-         "usr/share/glib-2.0/schemas/10_unity_greeter_background.gschema.override"
          "usr/share/gtksourceview-3.0/language-specs/fish.lang"
          "usr/share/nano/fish.nanorc"; do
   sudo ln -v $MYCONFIG_DIR/$i /$i
 done
 
 for i in "apt/apt.conf.d"
+         "fish"
          "fish/functions"; do
   sudo ln -v $MYCONFIG_DIR/etc/$i/* /etc/$i/
 done
 
 for i in "apt-fast.conf"
          "bash.bashrc"
-         "lightdm/lightdm-unity-greeter.conf"
          "login.defs"
          "nanorc"
          "sudoers"; do
   sudo ln -fv $MYCONFIG_DIR/etc/$i /etc/$i
-done
-
-for i in "fish"
-         "lightdm/lightdm.conf.d"; do
-  sudo ln -fv $MYCONFIG_DIR/my-config/etc/$i/* /etc/$i/
 done
 
 sudo fish --command="source /etc/fish/functions/fundle.fish; fundle install"
