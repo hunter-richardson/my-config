c7d3f5f2b49ab5d01ab8be16a25668907b7b25b6
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2018-10-04 09:10:12 -0500

    Correct installation typos and consistency

--- users.sh
+++ users.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Run /path/to/repo/install.sh first!
 sudo updatedb
-set MYCONFIG_DIR=$(dirname $(locate -eq 'my-config/.git' | head -1))
+set MYCONFIG_DIR=$(command dirname $(command locate -eq 'my-config/.git' | command head -1))
 
 sudo groupadd dev
 sudo groupadd user
@@ -9,7 +9,7 @@ sudo usermod -a -G user,dev,root,ssh hunter-adm
 sudo usermod -a -G user,dev,root,ssh root
 sudo usermod -a -G user,dev,sudo,ssh hunter
 for u in $(members dev); do
-  command set home_dir=$(command getent passwd $i | command cut -d':' -f6 | command tail -c +2)
+  home_dir=$(command getent passwd $i | command cut -d':' -f6 | command tail -c +2)
   sudo mkdir -p /$home_dir/.config/git
   sudo ln -v $MYCONFIG_DIR/$home_dir/Pictures/* /$home_dir/Pictures/
   sudo ln -v $MYCONFIG_DIR/$home_dir/.config/git/config /$home_dir/.config/git/config
