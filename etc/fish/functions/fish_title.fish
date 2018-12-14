cc6789b957455f5808b9ac5930bdff047a3a6337
Hunter Richardson <hrichardson8@gmail.com>
2018-12-13 22:00:21 -0600

    Complete separation of Ubuntu and Cygwin shell configurations

--- etc/fish/functions/fish_title.fish
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/fish
-
-function fish_title -d 'write the terminal title'
-  printf '%s' $_
-end
--- etc/fish/functions/refish.fish
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/fish
-
-function refish -d 'reload fish configuration'
-  builtin test -e ~/.config/fish/config.fish;
-    and source ~/.config/fish/config.fish;
-    or  source /etc/fish3/config.fish
-end
