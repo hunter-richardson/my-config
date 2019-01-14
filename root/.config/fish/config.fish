047d8238881907695fb5ed06edc4753d8471dfa4
Hunter Richardson <hrichardson8@gmail.com>
2019-01-13 21:50:02 -0600

    Configure installation with new SSD, updated software, user michelle

--- root/.config/fish/config.fish
+++ root/.config/fish/config.fish
@@ -1,3 +1,9 @@
 #!/usr/bin/fish
 
-source /etc/fish/config.fish
+fundle plugin 'edc/bass'
+fundle plugin 'oh-my-fish/plugin-await'
+fundle plugin 'oh-my-fish/plugin-balias'
+fundle plugin 'oh-my-fish/plugin-thefuck'
+fundle plugin 'tuvistavie/oh-my-fish-core'
+fundle init
+
--- root/config/fish/config.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/fish
-
-fundle plugin 'edc/bass'
-fundle plugin 'oh-my-fish/plugin-await'
-fundle plugin 'oh-my-fish/plugin-balias'
-fundle plugin 'oh-my-fish/plugin-thefuck'
-fundle plugin 'tuvistavie/oh-my-fish-core'
-fundle init
-
