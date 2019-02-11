7317a7c83b2c8f58a39a46df592174578e9a7309
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2019-02-11 17:39:14 -0600

    Update installation script

--- misc.sh
+++ misc.sh
@@ -1,48 +1,39 @@
 #!/bin/bash
 # Run /path/to/repo/users.sh first!
-sudo updatedb
-set MYCONFIG_DIR=$(dirname $(locate -eq 'my-config/.git' | head -1))
+set MYCONFIG_DIR=$(dirname ${BASH_SOURCE[0]})
 
 sudo tlp start
-cd $MYCONFIG
 sudo $(dirname $MYCONFIG_DIR)/matcha/Install
 
 sudo xinput set-button-map 11 1 2 3 4 5 6 7 0 0
 
-for i in "fish/functions"
-         "fish/fundle/edc"
-         "fish/fundle/oh-my-fish"
-         "fish/fundle/tuvistavie"
-         "skel/Pictures/screenshots"
-         "skel/.config/git"; do
+for i in 'fish/conf.d/functions/fundle'
+         'skel/Pictures/screenshots'; do
   sudo mkdir -p /etc/$i
 done
 
-for i in "etc/skel/.config/git/config"
-         "etc/tmux.conf"
-         "usr/local/sbin/adduser.local"
-         "usr/share/gtksourceview-3.0/language-specs/fish.lang"
-         "usr/share/nano/fish.nanorc"; do
-  sudo ln -v $MYCONFIG_DIR/$i /$i
+for i in 'etc/apt/apt.conf.d'
+         'etc/fish'
+         'etc/fish/conf.d'
+         'etc/fish/conf.d/completions'
+         'etc/fish/conf.d/functions'
+         'usr/local/sbin'; do
+  sudo ln -v $MYCONFIG_DIR/$i/* /$i/
 done
 
-for i in "apt/apt.conf.d"
-         "fish"
-         "fish/functions"; do
-  sudo ln -v $MYCONFIG_DIR/etc/$i/* /etc/$i/
+for i in 'apt-fast.conf'
+         'bash.bashrc'
+         'login.defs'
+         'nanorc'
+         'sudoers'; do
+  sudo ln -fv $MYCONFIG_DIR/etc/$i /etc/
 done
 
-for i in "apt-fast.conf"
-         "bash.bashrc"
-         "login.defs"
-         "nanorc"
-         "sudoers"; do
-  sudo ln -fv $MYCONFIG_DIR/etc/$i /etc/$i
+sudo fish --command="source /etc/fish/conf.d/functions/fundle.fish; fundle install"
+for i in 'edc/bass'
+         'oh-my-fish/plugin-*'
+         'tuvistavie/oh-my-fish-core'; do
+  sudo ln -v /root/.config/fish/fundle/$i/functions/*.fish /etc/fish/conf.d/functions/fundle/
 done
 
-sudo fish --command="source /etc/fish/functions/fundle.fish; fundle install"
-for i in "edc/bass"
-         "oh-my-fish/plugin-*"
-         "tuvistavie/oh-my-fish-core"; do
-  sudo ln -v /root/.config/fish/fundle/$i/functions/*.fish /etc/fish/fundle/$(echo $i | cut -d'/' -f1)
-done
+# now edit /etc/fish/config.fish to reference /etc/fish/conf.d/functions/fundle/*.fish
