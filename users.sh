9a41672eecba5c4897ac82451693d1fc0b74d327
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2019-02-11 17:43:33 -0600

    Update installation script

--- users.sh
+++ users.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 # Run /path/to/repo/install.sh first!
 sudo updatedb
-set MYCONFIG_DIR=$(command dirname $(command locate -eq 'my-config/.git' | command head -1))
+set MYCONFIG_DIR=$(dirname ${BASH_SOURCE[0]})
 
 sudo groupadd dev
 sudo groupadd user
 sudo usermod -a -G dev,root,ssh root
 sudo usermod -a -G user,dev,sudo,ssh hunter
 sudo usermod -a -G user,dev,root,ssh hunter-adm
-for u in $(members dev); do
-  home_dir=$(command getent passwd $i | command cut -d':' -f6 | command tail -c +2)
-  sudo mkdir -p /$home_dir/.config/git
-  sudo ln -v $MYCONFIG_DIR/$home_dir/Pictures/* /$home_dir/Pictures/
-  sudo ln -v $MYCONFIG_DIR/$home_dir/.config/git/config /$home_dir/.config/git/config
+for u in $(members dev)
+do
+  sudo ln -v $MYCONFIG_DIR/$(command getent passwd $i | command cut -d':' -f6 | command tail -c +2)/Pictures/* /$home_dir/Pictures/
 done
