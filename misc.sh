907511dd735c0dfc5a1161bceba1d40d6776ce51
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2019-02-13 12:50:28 -0600

    Update installation script

--- misc.sh
+++ misc.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 # Run /path/to/repo/users.sh first!
-set MYCONFIG_DIR=$(dirname ${BASH_SOURCE[0]})
-
 sudo tlp start
-sudo $(dirname $MYCONFIG_DIR)/matcha/Install
-
+sudo updatedb
+set MYCONFIG_DIR=$(sudo locate -ei --limit=1 'my-config')
+sudo $(sudo locate -ei --limit=1 'matcha')/Install
 sudo xinput set-button-map 11 1 2 3 4 5 6 7 0 0
-
-for i in 'fish/conf.d/functions/fundle' 'skel/Pictures/screenshots'
-do
-  sudo mkdir -p /etc/$i
-done
-
-for i in 'etc/apt/apt.conf.d' 'etc/fish' 'etc/fish/conf.d' 'etc/fish/conf.d/completions' 'etc/fish/conf.d/functions' 'usr/local/sbin'
+sudo mkdir -p /etc/skel/Pictures/screenshots
+for i in 'etc/apt/apt.conf.d' 'usr/local/sbin'
 do
   sudo ln -v $MYCONFIG_DIR/$i/* /$i/
 done
 
-for i in 'apt-fast.conf' 'bash.bashrc' 'login.defs' 'nanorc' 'sudoers'
+for i in $MYCONFIG_DIR/*
 do
-  sudo ln -fv $MYCONFIG_DIR/etc/$i /etc/
+  [ -f $i ] && ( sudo ln -fv $MYCONFIG_DIR/etc/$i /etc/ )
 done
