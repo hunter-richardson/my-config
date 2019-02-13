ef685d38e5f8b4f32db055d36d9b9c0379775955
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2019-02-13 14:17:06 -0600

    Add errno, grc, sublime, and xdg fish plugins

--- root/.config/fish/config.fish
+++ root/.config/fish/config.fish
@@ -3,7 +3,11 @@
 fundle plugin 'edc/bass'
 fundle plugin 'oh-my-fish/plugin-await'
 fundle plugin 'oh-my-fish/plugin-balias'
+fundle plugin 'oh-my-fish/plugin-errno'
+fundle plugin 'oh-my-fish/plugin-grc'
+fundle plugin 'oh-my-fish/plugin-sublime'
 fundle plugin 'oh-my-fish/plugin-thefuck'
+fundle plugin 'oh-my-fish/plugin-xdg'
 fundle plugin 'tuvistavie/oh-my-fish-core'
 fundle init
 
