d9c3b64cd008bed6ed1a1a6b72988db5b3478fe8
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2019-02-16 13:43:58 -0600

    Update installation

--- install.sh
+++ install.sh
@@ -41,11 +41,12 @@ do
   sudo apt $i -y
 done
 
+sudo snap install $(command cat $MYCONFIG_DIR/dpkg.snap)
+
 command sh -c "$(sudo curl -fLSs https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
 builtin eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 builtin printf "builtin eval \$($(brew --prefix)/bin/brew shellenv)" | sudo tee -a /etc/bash.bashrc
-sudo brew install -v $(command cat $MYCONFIG_DIR/dpkg.brew) && sudo brew tap -v $(cat $MYCONFIG_DIR/dpkg.tap)
-sudo snap install $(command cat $MYCONFIG_DIR/dpkg.snap)
+sudo brew install -v $(command cat $MYCONFIG_DIR/dpkg.brew)
 
 for i in $(cat $MYCONFIG_DIR/dpkg.raw)
 do
