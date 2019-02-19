d7e0e1551b2d2773c6d81881d1604acfb78a2409
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2019-02-19 15:30:59 -0600

    Use builtins/commands in scripts where possible

--- root/.config/fish/config.fish
+++ root/.config/fish/config.fish
@@ -1,36 +1,43 @@
 #!/usr/bin/fish
 
-fundle plugin 'edc/bass'
-fundle plugin 'decors/fish-colored-man'
-fundle plugin 'decors/fish-source-highlight'
-fundle plugin 'hunter-richardson/fish-humanize-duration'
-fundle plugin 'hunter-richardson/done'
-fundle plugin 'laughedelic/pisces'
-fundle plugin 'oh-my-fish/plugin-await'
-fundle plugin 'oh-my-fish/plugin-balias'
-fundle plugin 'oh-my-fish/plugin-errno'
-fundle plugin 'oh-my-fish/plugin-grc'
-fundle plugin 'oh-my-fish/plugin-sublime'
-fundle plugin 'oh-my-fish/plugin-thefuck'
-fundle plugin 'oh-my-fish/plugin-xdg'
-fundle plugin 'tuvistavie/oh-my-fish-core'
-fundle init
+builtin test -f /root/.config/fish/completions/fundle.fish;
+  and builtin source /root/.config/fish/completions/fundle.fish
 
-builtin set --local new_fundle_plugin (builtin test ! -d /root/.config/fish/fundle)
-builtin test $new_fundle_plugin;
-  or for i in (fundle list | command grep -v https://github.com)
-       builtin test $new_fundle_plugin;
-         and builtin break;
-         or  builtin set --local new_fundle_plugin (builtin test ! -d /root/.config/fish/fundle/$i)
-     end
+if builtin test -f /root/.config/fish/functions/fundle.fish
+  builtin source /root/.config/fish/functions/fundle/alias.fish
 
-builtin test $new_fundle_plugin;
-  and fundle install;
-  and for i in (fundle list | grep -v https://github.com)
-        builtin printf 'load plugin %s\n' $i | builtin string replace / :
-        for f in functions completions
-          test -d /root/.config/fish/fundle/$i/$f;
-            and chmod a+x /root/.config/fish/fundle/$i/$f/*;
-            and ln -v /root/.config/fish/fundle/$i/$f/* /etc/fish/$f/
+  fundle plugin 'edc/bass'
+  fundle plugin 'decors/fish-colored-man'
+  fundle plugin 'decors/fish-source-highlight'
+  fundle plugin 'hunter-richardson/fish-humanize-duration'
+  fundle plugin 'hunter-richardson/done'
+  fundle plugin 'laughedelic/pisces'
+  fundle plugin 'oh-my-fish/plugin-await'
+  fundle plugin 'oh-my-fish/plugin-balias'
+  fundle plugin 'oh-my-fish/plugin-errno'
+  fundle plugin 'oh-my-fish/plugin-grc'
+  fundle plugin 'oh-my-fish/plugin-sublime'
+  fundle plugin 'oh-my-fish/plugin-thefuck'
+  fundle plugin 'oh-my-fish/plugin-xdg'
+  fundle plugin 'tuvistavie/oh-my-fish-core'
+  fundle init
+
+  builtin set --local new_fundle_plugin (builtin test ! -d /root/.config/fish/fundle)
+  builtin test $new_fundle_plugin;
+    or for i in (fundle list | command grep -v https://github.com)
+         builtin test $new_fundle_plugin;
+           and builtin break;
+           or  builtin set --local new_fundle_plugin (builtin test ! -d /root/.config/fish/fundle/$i)
+       end
+
+  builtin test $new_fundle_plugin;
+    and fundle install;
+    and for i in (fundle list | command grep -v https://github.com)
+          builtin printf 'load plugin %s\n' $i | builtin string replace / :
+          for f in functions completions
+            builitn test -d /root/.config/fish/fundle/$i/$f;
+              and command chmod a+x /root/.config/fish/fundle/$i/$f/*;
+              and command ln -v /root/.config/fish/fundle/$i/$f/* /etc/fish/$f/
+          end
         end
-      end
+end
