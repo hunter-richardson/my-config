5b84e10006024ef6a493383e95b5f3124a4017af
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2019-02-19 11:22:41 -0600

    Install Fantasy Grounds by CLI for multiple-users

---
+++ game.sh
@@ -0,0 +1,11 @@
+sudo adduser --disabled-password wine
+sudo usermod -a -G wine-adm,wine-user hunter-adm
+sudo usermod -a -G wine-user hunter
+sudo usermod -a -G wine-user michelle
+
+for i in $(command members wine-user)
+do
+  [ -z $(command cat ~$i/.profile | command grep i '^command xhost +local:wine$') ] && builtin printf 'command xhost +local:wine' | command tee -a ~$i/.profile
+done
+
+wget https://fantasygrounds.com/filelibrary/FGWebInstall.exe | sudo --user='wine' --command='wine start - &'
