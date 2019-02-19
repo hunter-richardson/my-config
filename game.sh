8c510ac4d4e4cd803f44c844dfd49be7e14b9d7b
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2019-02-19 12:07:36 -0600

    Correct typos in installation

--- game.sh
+++ game.sh
@@ -5,7 +5,9 @@ sudo usermod -a -G wine-user michelle
 
 for i in $(command members wine-user)
 do
-  [ -z $(command cat ~$i/.profile | command grep i '^command xhost +local:wine$') ] && builtin printf 'command xhost +local:wine' | command tee -a ~$i/.profile
+  home=$(command getent passwd $i | command cut -d':' -f6)
+  [ -n "$home" -a ! $(command grep -i '^(command )?xhost +local:wine' $home/.profile) ] && builtin printf 'command xhost +local:wine' | command tee -a $home/.profile
+  builtin unset home
 done
 
 command wget https://fantasygrounds.com/filelibrary/FGWebInstall.exe | sudo --user='wine' --command='command wine start - &'
