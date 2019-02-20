00a3c53fe867b9a19bb22f442052fed2e0698114
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2019-02-20 14:05:18 -0600

    Comment installation script

--- misc.sh
+++ misc.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # Run /path/to/repo/users.sh first!
-sudo tlp start
-sudo updatedb
-MYCONFIG_DIR=$(sudo locate -ei --limit=1 'my-config')
-sudo $(sudo locate -ei --limit=1 'matcha')/Install
-sudo xinput set-button-map 11 1 2 3 4 5 6 7 0 0
+sudo tlp start #initialize tlp
+sudo updatedb #refresh directory database
+MYCONFIG_DIR=$(sudo locate -ei --limit=1 'my-config') #locate my-config repo
+sudo $(sudo locate -ei --limit=1 'matcha')/Install #install shell theme
+sudo xinput set-button-map 11 1 2 3 4 5 6 7 0 0 #disable mouse forward and back buttons
 sudo mkdir -p /etc/skel/Pictures/screenshots
 for i in 'etc/apt/apt.conf.d' 'usr/local/sbin'
 do
-  sudo ln -v $MYCONFIG_DIR/$i/* /$i/
+  sudo ln -v $MYCONFIG_DIR/$i/* /$i/ #link configuration files
 done
 
 for i in $MYCONFIG_DIR/*
 do
-  [ -f $i ] && ( sudo ln -fv $MYCONFIG_DIR/etc/$i /etc/ )
+  [ -f $i ] && ( sudo ln -fv $MYCONFIG_DIR/etc/$i /etc/ ) #link configuration files
 done
