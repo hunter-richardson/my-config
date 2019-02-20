029480b0505d78edd7981ea59efd2795f9977e24
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2019-02-20 14:37:04 -0600

    Comment installation script

--- themes.sh
+++ themes.sh
@@ -1,7 +1,7 @@
 # download https://opendesktop.org/c/1460734834
-cd ~/Downloads
-dtrx -nv ~/Downloads/163336-DMZhaloRP.tar.gz
-sudo mkdir -p /usr/share/icons/DMZhaloR32
-sudo rsync -ADhorX ~/Downloads/163336-DMZhaloRP/DMZhaloRP/DMZhaloR32/* /usr/share/icons/DMZhaloR32/
-sudo ln -fs /usr/share/icons/DMZhaloR24/cursor.theme /etc/alternatives/x-cursor-theme
-sudo srm -lrvz ~/Downloads/163336-DMZhaloRP ~/Downloads/163336-DMZhaloRP.tar.gz
+cd ~/Downloads #dir to downloads
+command dtrx -nv ~/Downloads/163336-DMZhaloRP.tar.gz #extract theme
+sudo mkdir -p /usr/share/icons/DMZhaloR32 #mkdir theme dir
+sudo rsync -ADhorX ~/Downloads/163336-DMZhaloRP/DMZhaloRP/DMZhaloR32/* /usr/share/icons/DMZhaloR32/ #copy theme to new dir
+sudo ln -fs /usr/share/icons/DMZhaloR24/cursor.theme /etc/alternatives/x-cursor-theme #link theme
+sudo srm -lrvz ~/Downloads/163336-DMZhaloRP ~/Downloads/163336-DMZhaloRP.tar.gz #destroy download
