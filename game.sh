ba765ba0edbfb752a517373ecf673f9a8a17d416
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2019-02-20 14:36:53 -0600

    Comment installation script

--- game.sh
+++ game.sh
@@ -1,13 +1,14 @@
-sudo adduser --disabled-password wine
-sudo usermod -a -G wine-adm,wine-user hunter-adm
-sudo usermod -a -G wine-user hunter
-sudo usermod -a -G wine-user michelle
+sudo adduser --disabled-password wine #add user wine
+sudo usermod -a -G wine-adm,wine-user hunter-adm #add wine-adm hunter-adm
+sudo usermod -a -G wine-user hunter #add wine-user hunter
+sudo usermod -a -G wine-user michelle #add wine-user michelle
 
 for i in $(command members wine-user)
 do
-  home=$(command getent passwd $i | command cut -d':' -f6)
-  [ -n "$home" ] && [ ! $(command grep -i '^(command )?xhost \+local:wine' $home/.profile) ] && builtin printf 'command xhost +local:wine' | command tee -a $home/.profile
-  builtin unset home
+  home=$(command getent passwd $i | command cut -d':' -f6) #locate user homedir
+  [ -n "$home" ] && [ ! $(command grep -i '^(command )?xhost \+local:wine' $home/.profile) ] && builtin printf 'command xhost +local:wine' | command tee -a $home/.profile #autosetup wine access
+  builtin unset home #free variable
 done
 
-command wget https://fantasygrounds.com/filelibrary/FGWebInstall.exe | sudo --user=wine --command='command wine start /dev/stdout &'
+command xhost +local:wine #setup wine access
+command curl -v https://fantasygrounds.com/filelibrary/FGWebInstall.exe | sudo --user=wine --command=$(bulitin printf 'command wine start /dev/stdout %s &' -) #download and install wine
