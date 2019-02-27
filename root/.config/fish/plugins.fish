fd12df15f42900fdd57cc6f6b3c400e58cef3508
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2019-02-27 11:49:50 -0600

    cut variables before grep

--- root/.config/fish/plugins.fish
+++ root/.config/fish/plugins.fish
@@ -4,7 +4,7 @@ if builtin test -f /root/.config/fish/conf.d/functions/fundle.fish
   builtin source /root/.config/fish/conf.d/functions/fundle.fish
   builtin test -f /root/.config/fish/conf.d/completions/fundle.fish;
     and builtin source /root/.config/fish/conf.d/completions/fundle.fish
-  for i in (builtin set -g | command grep -E '__fundle.*_plugin' | command grep -v history | command cut -d' ' -f1)
+  for i in (builtin set -g | command cut -d' ' -f1 | command grep -E '^__fundle.*_plugin$' | command grep -v history)
     set -e $i
   end
 
