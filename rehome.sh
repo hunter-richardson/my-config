152774324b4d7c5c47196a55400abd210da20ddb
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2019-09-19 12:58:53 -0500

    User internal SSD for userspace, not /home

--- rehome.sh
+++
@@ -1,4 +0,0 @@
-sudo mkdir /apartment # create disk directory
-sudo mkfs.ext4 /dev/sdb # recreate filesystem on device desired
-sudo mount -v /dev/sdb /apartment # mount device onto directory
-grep 'home' /etc/mtab | sudo tee -a /etc/fstab # ensure system keeps new mount
