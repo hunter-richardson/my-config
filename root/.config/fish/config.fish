ce5273edde6d16293d1c9ddf7abf5ff500bdbf3e
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2019-09-20 10:01:18 -0500

    Shuffle fundle plugin load order

--- root/.config/fish/config.fish
+++ root/.config/fish/config.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/fish
 
 builtin source /root/.config/fish/conf.d/{completions,functions}/fundle.fish
-for i in (command grep -Ev '^#' /root/.config/fish/fundle.plugins)
+for i in (command grep -Ev '^#' /root/.config/fish/fundle.plugins | command shuf)
   fundle plugin $i;
     and builtin printf 'load plugin %s\n' (builtin string replace / : $i)
 end;
