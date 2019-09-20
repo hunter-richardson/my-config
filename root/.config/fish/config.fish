710826f55cbcbab64c714a390f9d0fbbab2345af
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2019-09-20 09:45:05 -0500

    Update root's fish config

--- root/.config/fish/config.fish
+++ root/.config/fish/config.fish
@@ -4,4 +4,5 @@ builtin source /root/.config/fish/conf.d/{completions,functions}/fundle.fish
 for i in (command grep -Ev '^#' /root/.config/fish/fundle.plugins)
   fundle plugin $i;
     and builtin printf 'load plugin %s\n' (builtin string replace / : $i)
-end
+end;
+  and fundle init
