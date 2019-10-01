64f6824ad2098c8473a5a0544c5d20a5dca77d47
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2019-10-01 10:01:04 -0500

    Beep on error in webfilter/policy check

--- home/michelle/test_filter.fish
+++ home/michelle/test_filter.fish
@@ -1,3 +1,4 @@
       command grep --color=always '\"IncognitoModeAvailability\"' /etc/{opt/chrome,chromium}/policies/managed/policy.json;
   and command grep --color=always '\"DisablePrivateBrowsing\"' /usr/{lib}/firefox/distribution/policies.json | command head -1;
-  and command google-chrome opendns.org/welcome
+  and command google-chrome opendns.org/welcome;
+  or  builtin printf '\a'
