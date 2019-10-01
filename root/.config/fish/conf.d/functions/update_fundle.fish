fda1970d659d947051308203c47d81e36cf6358a
Hunter Richardson <hrichardson8@gmail.com>
2019-10-01 18:56:15 -0500

    Reformat output in update_fundle

--- root/.config/fish/conf.d/functions/update_fundle.fish
+++ root/.config/fish/conf.d/functions/update_fundle.fish
@@ -4,10 +4,10 @@ for i in (builtin set -g | command grep '__fundle.*_plugin' | command cut -d' ' 
   builtin test -n "$i";
     and builtin set -e $i;
     or  builtin continue
-end
+end;
   and for i in (command find /root -type f -name fundle.fish | command shuf)
         builtin source $i;
-          and builtin printf 'source GITHUB/danhper:fundle fundle %s\n' (command basename (command dirname $i))
+          and builtin printf 'load GITHUB/danhper:fundle fundle %s\n' (command basename (command dirname $i))
       end;
   and for i in (command grep -Ev '^#' /root/.config/fish/fundle.plugins | command shuf)
         fundle plugin $i
@@ -19,7 +19,7 @@ fundle install | builtin string replace / : | builtin string replace 'installed 
   and for i in (fundle list --short | command shuf)
         fundle update $i | builtin string replace / : | builtin string replace hunter-richardson \$ME
       end;
-  and for i in (command find /root/.config/fish/fundle/**/{comple,func}tions -type f -name '*.fish' | command shuf)
-        command ln -f $i /etc/fish/conf.d/(command basename (command dirname $i))/;
-          and builtin printf 'global /etc/fish/conf.d/%s/%s => %s %s fish %s\n' (command basename (command dirname $i)) (command basename $i | builtin string replace s '') (builtin printf '%s' $i | command cut -d/ -f6,7 | builtin string replace / : | builtin string replace hunter-richardson \$ME) (command basename $i .fish) (command basename (command dirname $i) | builtin string replace s '')
-      end
+for i in (command ls -1 /root/.config/fish/fundle/**/{comple,func}tions/*.fish | command shuf)
+  command ln -f $i /etc/fish/conf.d/(command basename (command dirname $i))/;
+    and builtin printf 'global /etc/fish/conf.d/%s/%s => %s/%s %s fish %s\n' (command basename (command dirname $i)) (command basename $i | builtin string replace s '') (builtin printf '%s\n' $__fundle_plugin_urls | command grep (builtin printf '%s' $i | command cut -d/ -f6,7) | command cut -d/ -f3 | command cut -d. -f1 | builtin string upper) (builtin printf '%s' $i | command cut -d/ -f6,7 | builtin string replace / : | builtin string replace hunter-richardson \$ME) (command basename $i .fish) (command basename (command dirname $i) | builtin string replace s '')
+end
