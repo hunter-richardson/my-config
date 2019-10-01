2410d85e3695c545b565fcbbf51b8a5332f4d62a
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2019-10-01 09:58:17 -0500

    Optimize webfilter/policy check

--- home/michelle/test_filter.fish
+++ home/michelle/test_filter.fish
@@ -1,3 +1,3 @@
-grep '\"IncognitoModeAvailability\"' /etc/{opt/chrome,chromium}/policies/managed/policy.json;
-  and grep '\"DisablePrivateBrowsing\"' /usr/lib/firefox/distribution/policies.json;
-  and google-chrome opendns.org/welcome
+      command grep --color=always '\"IncognitoModeAvailability\"' /etc/{opt/chrome,chromium}/policies/managed/policy.json;
+  and command grep --color=always '\"DisablePrivateBrowsing\"' /usr/{lib}/firefox/distribution/policies.json | command head -1;
+  and command google-chrome opendns.org/welcome
