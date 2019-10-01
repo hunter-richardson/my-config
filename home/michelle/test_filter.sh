9809b89fe6f4ceb47c37f29cf3279499a47ba660
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2019-10-01 10:36:01 -0500

    Optimize webfilter/policy check

--- home/michelle/test_filter.sh
+++ home/michelle/test_filter.sh
@@ -1,4 +1,5 @@
-     command grep --color=always '\"IncognitoModeAvailability\"' /etc/{opt/chrome,chromium}/policies/managed/policy.json \
-  && command grep --color=always '\"DisablePrivateBrowsing\"' /usr/{lib}/firefox/distribution/policies.json | command head -1 \
-  && command google-chrome opendns.org/welcome
-  || builtin printf '\a'
+for i in $(command ls -1 /etc/chromium/policies/managed/policies.json /etc/opt/chrome/policies/managed/policies.json)
+  builtin printf '\e[1m;34m%s\e[0m: \e[1m;31m%s\e[0m' $i $(command grep '\"IncognitoModeAvailability\"' $i) || builtin printf '\a'
+end && for i in $(command ls -1 /usr/lib/firefox/distribution/policies.json)
+  builtin printf '\e[1m;34m%s\e[0m: \e[1m;31m%s\e[0m' $i $(command grep '\"DisablePrivateBrowsing\"' $i) || builtin printf '\a'
+end && command google-chrome opendns.org/welcome || builtin printf '\a'
