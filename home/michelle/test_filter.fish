4e85a45099491c9317f93440a66c3666f63dbaa1
Hunter Richardson <hrichardson8@gmail.com>
2019-10-01 20:36:15 -0500

    Correct output in webfilter/policy check-script

--- home/michelle/test_filter.fish
+++ home/michelle/test_filter.fish
@@ -1,9 +1,9 @@
       for i in (command ls -1 /etc/{opt/chrome,chromium}/policies/managed/policy.json)
-        builtin printf '%s%s%s%s: %s%s%s%s' $bold $blue $i $normal $bold $red (command grep '\"IncognitoModeAvailability\"' $i) $normal;
+        builtin printf '%s%s%s%s: %s%s%s%s\n' $bold $blue $i $normal $bold $red (command grep '\"IncognitoModeAvailability\"' $i) $normal;
           or  builtin printf '\a'
       end;
   and for i in (command ls -1 /usr/lib/firefox/distribution/policies.json)
-        builtin printf '%s%s%s%s: %s%s%s%s' $bold $blue $i $normal $bold $red (command grep '\"DisablePrivateBrowsing\"' $i) $normal;
+        builtin printf '%s%s%s%s: %s%s%s%s\n' $bold $blue $i $normal $bold $red (command grep '\"DisablePrivateBrowsing\"' $i) $normal;
           or  builtin printf '\a'
       end;
   and command google-chrome opendns.org/welcome
