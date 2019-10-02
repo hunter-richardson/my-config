8dff0af51e4412e6c2b3706c71b215ef1af49bde
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2019-10-02 16:35:48 -0500

    Obsoleted `root`'s fish config

--- root/.config/fish/config.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/fish
-
-builtin source /root/.config/fish/conf.d/functions/fundle.fish
-builtin source /root/.config/fish/conf.d/completions/fundle.fish
-for i in (command grep -Ev '^#' /root/.config/fish/fundle.plugins | command shuf)
-  fundle plugin $i;
-    and builtin printf 'load plugin %s\n' (builtin string replace / : $i)
-end;
-  and fundle init
